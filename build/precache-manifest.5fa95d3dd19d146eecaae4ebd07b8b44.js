bbdcbcee6abc1ef48f2f5aff17412331676bed09
Titas Nath <79986094+trip0le@users.noreply.github.com>
2021-10-12 15:59:29 +0530

    Add files via upload

---
+++ build/precache-manifest.5fa95d3dd19d146eecaae4ebd07b8b44.js
@@ -0,0 +1,26 @@
+self.__precacheManifest = [
+  {
+    "revision": "ba6890dfee2ab28fce2d",
+    "url": "/static/css/main.e172c0eb.chunk.css"
+  },
+  {
+    "revision": "ba6890dfee2ab28fce2d",
+    "url": "/static/js/main.ba6890df.chunk.js"
+  },
+  {
+    "revision": "cf882c645a4092c7392e",
+    "url": "/static/css/1.57822213.chunk.css"
+  },
+  {
+    "revision": "cf882c645a4092c7392e",
+    "url": "/static/js/1.cf882c64.chunk.js"
+  },
+  {
+    "revision": "229c360febb4351a89df",
+    "url": "/static/js/runtime~main.229c360f.js"
+  },
+  {
+    "revision": "d0eeca8728d8daa9ddcf613b7669c9aa",
+    "url": "/index.html"
+  }
+];
